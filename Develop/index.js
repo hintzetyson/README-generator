d4397593c7084ef7df54f2239e15e4b0009fb6ab
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-02 10:52:15 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,3 @@
-
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
@@ -6,7 +5,62 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 
 
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is your Github name?',
+        name: 'githubName'
+    },
+    {
+        type: 'input',
+        message: 'What is the name of your Github repo?',
+        name: 'githubRepo',
+    },
+    {
+        type: 'confirm',
+        message: 'Would you like to use your real name on the README?',
+        name: 'realName'
+    },
+    {
+        type: 'input',
+        message: 'Please make a short description for your project',
+        name: 'description',
+    },
+    {
+        type: 'confirm',
+        message: 'Would like a table of contents?',
+        name: 'table',
+    },
+    {
+        type: 'input',
+        message: 'What command does the user need to run to install needed dependencies?',
+        name: 'install',
+        default: 'npm i',
+    },
+    {
+        type: 'input',
+        message: 'How does one use your product?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'What type of license would you like to use?',
+        name: 'license',
+        default: 'Use arrow key to navigate',
+        choices: ['MIT', 'GPL 3.0', 'APACHE 2.0', 'BSD 3', 'No license']
+    },
+    {
+        type: 'confirm',
+        message: 'Would you like a list on how to contribute?',
+        name: 'contributeTrueOrFalse'
+    }, {
+        when: function (response) {
+          return response.contributeTrueOrFalse;
+        },
+        type: 'input',
+        message: 'How many steps would you like to list?',
+        name: 'contributeSteps',
+        validate: numberValidation
+    }
 ];
 
 function writeToFile(fileName, data) {
