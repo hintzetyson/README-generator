75a68072a8be533433f48a3ab435ed4e234577b6
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-02 12:27:25 -0600

    Added table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -7,7 +7,28 @@ var markdown = `# ${data.githubRepo.replace(/-/g, " ")}\n`
 markdown += `\n[![repo size](https://img.shields.io/github/repo-size/${data.githubName}/${data.githubRepo})](https://github.com/${data.githubName}/${data.githubRepo})`;
 
 
+//adding a table of contents
 
+if (data.table) {
+  string += '\n## Table of contents\n';
+
+  string += '\n* [Installation](#installation)\n';
+  string += '\n* [Usage](#usage)\n';
+
+  //Only appears if this exists
+  if (data.contributeTrueOrFalse) {
+    string += '\n* [Contributing](#contributing)\n';
+  }
+
+  //Only appears if tests are wanted
+  if (data.testsTrueorFalse) {
+    string += '\n* [Tests](#tests)\n';
+  }
+
+  
+  string += '\n* [Questions](#questions)\n'
+  string += '\n* [License](#license)\n'
+}
 
   return `
 # ${data.title}
