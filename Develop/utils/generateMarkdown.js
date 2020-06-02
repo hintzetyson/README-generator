831f52724418b607ab37d860630019a3a35a672c
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-02 12:24:03 -0600

    started work on the markdown generator

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,14 @@
 function generateMarkdown(data) {
+
+//this takes away any spaces, the g means globally
+var markdown = `# ${data.githubRepo.replace(/-/g, " ")}\n`
+
+//Badge for the repo, this was found on the website how to get this
+markdown += `\n[![repo size](https://img.shields.io/github/repo-size/${data.githubName}/${data.githubRepo})](https://github.com/${data.githubName}/${data.githubRepo})`;
+
+
+
+
   return `
 # ${data.title}
 
