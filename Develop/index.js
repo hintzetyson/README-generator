3e449ba3724e63fc1880b259f12e3dacf5cce40a
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-02 10:49:17 -0600

    added dependencies in index.js

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
